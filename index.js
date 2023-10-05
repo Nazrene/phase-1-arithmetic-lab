bd8c583b4a43c01779e1b46d7066ae0f28c46792
Naz <mercy.kamau@student.moringaschool.com>
2023-10-05 17:19:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2
 
-// Write your code here
+const random = Math.ceil(Math.random () * 100 ) ;
+
+const num3 = 25;
+const num4 = 7;
+const mod = num3 % num4;
+
+const max = Math.max(10, 15, 20, 5) ;
+
+console.log("multiply:", multiply);
+console.log("random:" ,random);
+console.log("mod:", mod);
+console.log("max:", max);// Write your code here
